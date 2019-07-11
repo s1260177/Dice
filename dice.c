92ea980f80a4da7ed7aa69ddf62c1fa7b4fdb607
s126yukun <52778944+s126yukun@users.noreply.github.com>
2019-07-11 19:55:18 +0900

    Update dice.c

--- dice.c
+++ dice.c
@@ -10,6 +10,9 @@ int main()
   first = rand()%6+1;
   second = rand()%6+1;
   total = first+second;
+   third = rand()%6+1;
+  fourth = rand()%6+1;
+  total2 = third+fourth;
 
   printf("What is your name?\n>");
   scanf("%s",name);
@@ -18,15 +21,15 @@ int main()
   printf("Die 1: %d\n",first);
   printf("Die 2: %d\n",second);
   printf("Total value: %d\n",total);
-  if(total>7) printf("%s won!\n"name);
-  else printf("%s lost.\n"name);
+  if(total>7) printf("%s won!\n",name);
+  else printf("%s lost.\n",name);
   
   printf("Rolling more dice\n");
-  printf("Die 1: %d\n",first);
-  printf("Die 2: %d\n",second);
-  printf("Total value: %d\n",total);
-  if(total>7) printf("%s won!\n"name);
-  else printf("%s lost.\n"name);
+  printf("Die 1: %d\n",third);
+  printf("Die 2: %d\n",fourth);
+  printf("Total value: %d\n",total2);
+  if(total2>7) printf("%s won!\n",name);
+  else printf("%s lost.\n",name);
 
   return 0;
 }
