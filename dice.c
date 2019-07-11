077234b232047371b32a4506737c7f1357a32b53
s126yukun <52778944+s126yukun@users.noreply.github.com>
2019-07-11 19:43:51 +0900

    Update dice.c

--- dice.c
+++ dice.c
@@ -20,6 +20,13 @@ int main()
   printf("Total value: %d\n",total);
   if(total>7) printf("%s won!\n"name);
   else printf("%s lost.\n"name);
+  
+  printf("Rolling more dice\n");
+  printf("Die 1: %d\n",first);
+  printf("Die 2: %d\n",second);
+  printf("Total value: %d\n",total);
+  if(total>7) printf("%s won!\n"name);
+  else printf("%s lost.\n"name);
 
   return 0;
 }
