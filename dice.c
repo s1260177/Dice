12857ee15c6438a3509af00fc6615c73e8a73401
s126yukun <52778944+s126yukun@users.noreply.github.com>
2019-07-11 19:16:37 +0900

    Update dice.c

--- dice.c
+++ dice.c
@@ -14,6 +14,8 @@ int main()
   printf("Die 1: %d\n",first);
   printf("Die 2: %d\n",second);
   printf("Total value: %d\n",total);
+  if(total>7) printf("You won!\n");
+  else printf("You lost.\n");
 
   return 0;
 }
