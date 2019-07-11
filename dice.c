ae6a7ead0301147cfddb14312061b25a6e3591de
s126yukun <52778944+s126yukun@users.noreply.github.com>
2019-07-11 19:12:32 +0900

    Update dice.c

--- dice.c
+++ dice.c
@@ -5,11 +5,15 @@
 int main()
 {
   int first,second,total;
+  char name[10];
   srand(time(NULL));
   first = rand()%6+1;
   second = rand()%6+1;
   total = first+second;
 
+  printf("What is your name?\n>");
+  scanf("%s",name);
+  printf("Hello, %s!\n",name);
   printf("Rolling the dice...\n");
   printf("Die 1: %d\n",first);
   printf("Die 2: %d\n",second);
